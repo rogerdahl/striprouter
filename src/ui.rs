aca54d30fa5e03a69dc9b737314b3767bd81077a
rogerdahl <git@dahlsys.com>
2024-01-14 13:05:42 -0700

    initial work

--- src/ui.rs
+++ src/ui.rs
@@ -1,24 +1,32 @@
 use crate::render::Render;
 use crate::MyApp;
 use eframe::epaint::{Color32, FontId};
-use egui::{Button, Context, FontData, FontDefinitions, FontFamily, RichText, TextStyle, Ui};
+use egui::WidgetText::RichText;
+use egui::{Align, Button, Context, FontData, FontDefinitions, FontFamily, Rect, TextStyle, Ui};
+use num_format::{Locale, ToFormattedString};
+use std::fmt::format;
 use std::sync::atomic::Ordering;
 use std::time::Instant;
 
 pub(crate) struct Controls {
     pub ms_per_frame: f32,
-    pub checked_total: f32,
+    pub checked_total: usize,
     pub checked_per_second: f32,
     pub wire_cost: i32,
     pub strip_cost: i32,
     pub via_cost: i32,
     pub cut_cost: i32,
     pub zoom: f32,
-    pub failed_routes_avg: usize,
-    pub layout_current_cost: usize,
+    // pub failed_routes_avg: usize,
+    // pub layout_current_cost: usize,
+    pub current_layout_completed_routes: usize,
+    pub current_layout_failed_routes: usize,
+    pub current_layout_cost: usize,
+
     pub best_layout_completed_routes: usize,
     pub best_layout_failed_routes: usize,
     pub best_layout_cost: usize,
+
     pub show_rats_nest: bool,
     pub show_only_failed: bool,
     pub show_current_layout: bool,
@@ -29,26 +37,33 @@ impl Default for Controls {
     fn default() -> Self {
         Self {
             ms_per_frame: 1.0,
-            checked_total: 0.0,
+            checked_total: 0,
             checked_per_second: 0.0,
             wire_cost: 1,
             strip_cost: 1,
             via_cost: 1,
             cut_cost: 1,
             zoom: 15.0,
-            failed_routes_avg: 0,
-            layout_current_cost: 0,
+
+            current_layout_completed_routes: 0,
+            current_layout_failed_routes: 0,
+            current_layout_cost: 0,
+
             best_layout_completed_routes: 0,
             best_layout_failed_routes: 0,
             best_layout_cost: 0,
+
             show_rats_nest: false,
             show_only_failed: false,
             show_current_layout: false,
+
             pause_router: false,
         }
     }
 }
 
+// #[rustfmt::skip::attributes(max_width)]
+#[rustfmt::skip]
 impl Controls {
     pub(crate) fn ui(&mut self, ctx: &egui::Context) {
         egui::SidePanel::left("control_panel").show(ctx, |ui| {
@@ -58,181 +73,143 @@ impl Controls {
 
                 ui.add_space(8.0);
 
-                ui.vertical_centered(|ui| {
-                    ui.heading("File");
-                });
+                egui::Grid::new("control-grid").show(ui, |ui| {
+                    Controls::header(ui, "File", true);
+                    Controls::section(ui, "Circuit file");
 
-                Controls::level_0_label(ui, "Circuit file");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("file-circuit-grid").show(ui, |ui| {
-                        // ui.add_sized(ui.available_size(), Button::new("Save to .circuit file"));
+                    ui.horizontal(|ui| {
+                        ui.label("    ");
                         if ui.button("Open").clicked() {
                             println!("Button was clicked!");
                         }
+                    });
+                    ui.end_row();
+
+                    ui.horizontal(|ui| {
+                        ui.label("    ");
                         if ui.button("Save").clicked() {
                             println!("Button was clicked!");
                         }
-                        ui.end_row();
                     });
-                });
+                    ui.end_row();
 
-                Controls::level_0_label(ui, "Best Layout");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("file-svg-grid").show(ui, |ui| {
-                        if ui.button("Save to .svg files").clicked() {
+                    // });
+                    ui.end_row();
+
+                    Controls::section(ui, "Best Layout");
+                    ui.horizontal(|ui| {
+                        ui.label("    ");
+                        if ui.button("Save to .svg").clicked() {
                             println!("Button was clicked!");
                         }
                     });
-                });
+                    ui.end_row();
 
-                ui.add_space(16.0);
+                    Controls::header(ui, "Layouts", false);
 
-                ui.vertical_centered(|ui| {
-                    ui.heading("Status");
-                });
+                    Controls::section(ui, "Total");
 
-                Controls::level_0_label(ui, "Total");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("total-grid").show(ui, |ui| {
-                        ui.label("Checked");
-                        Controls::highlighted_label(ui, &format!("{}", self.checked_total));
-                        ui.end_row();
+                    Controls::name_int(ui, "Checked", self.checked_total);
+                    Controls::name_float(ui, "Checked/s", self.checked_per_second);
 
-                        ui.label("Checked/s");
-                        Controls::highlighted_label(ui, &format!("{}", self.checked_per_second));
-                        ui.end_row();
-                    });
-                });
+                    Controls::section(ui, "Current");
 
-                Controls::level_0_label(ui, "Current Layout");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("current-cost-grid").show(ui, |ui| {
-                        ui.label("Completed");
-                        Controls::highlighted_label(ui, &format!("{}", self.ms_per_frame));
-                        ui.end_row();
+                    Controls::name_int(ui, "Completed", self.current_layout_completed_routes);
+                    Controls::name_int(ui, "Failed Avg", self.current_layout_failed_routes);
+                    Controls::name_int(ui, "Cost", self.current_layout_cost);
 
-                        ui.label("Failed Avg");
-                        Controls::highlighted_label(ui, &format!("{}", self.failed_routes_avg));
-                        ui.end_row();
+                    Controls::section(ui, "Best");
 
-                        ui.label("Cost");
-                        Controls::highlighted_label(ui, &format!("{}", self.layout_current_cost));
-                        ui.end_row();
-                    });
-                });
+                    Controls::name_int(ui, "Completed", self.best_layout_completed_routes);
+                    Controls::name_int(ui, "Failed", self.best_layout_failed_routes);
+                    Controls::name_int(ui, "Cost", self.best_layout_cost);
 
-                Controls::level_0_label(ui, "Best Layout");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("best-cost-grid").show(ui, |ui| {
-                        ui.label("Completed");
-                        Controls::highlighted_label(ui, &format!("{}", self.best_layout_completed_routes));
-                        ui.end_row();
+                    Controls::header(ui, "Router", false);
 
-                        ui.label("Failed");
-                        Controls::highlighted_label(ui, &format!("{}", self.best_layout_failed_routes));
-                        ui.end_row();
+                    Controls::section(ui, "Costs");
 
-                        ui.label("Cost");
-                        Controls::highlighted_label(ui, &format!("{}", self.best_layout_cost));
-                        ui.end_row();
-                    });
-                });
+                    Controls::name_widget(ui, "Wire", egui::DragValue::new(&mut self.wire_cost).clamp_range(1..=100));
+                    Controls::name_widget(ui, "Strip", egui::DragValue::new(&mut self.strip_cost).clamp_range(1..=100));
+                    Controls::name_widget(ui, "Via", egui::DragValue::new(&mut self.via_cost).clamp_range(1..=100));
+                    Controls::name_widget(ui, "Cut", egui::DragValue::new(&mut self.cut_cost).clamp_range(1..=100));
 
-                Controls::level_0_label(ui, "Render");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("render-grid").show(ui, |ui| {
-                        ui.label("ms/frame");
-                        Controls::highlighted_label(ui, &format!("{}", self.ms_per_frame));
-                        ui.end_row();
-                    });
-                });
+                    Controls::section(ui, "Display");
 
-                ui.add_space(16.0);
+                    Controls::name_widget(ui, "Rat's Nest", egui::Checkbox::new(&mut self.show_rats_nest, ""));
+                    Controls::name_widget(ui, "Current", egui::Checkbox::new(&mut self.show_only_failed, ""));
+                    Controls::name_widget(ui, "Only Failed", egui::Checkbox::new(&mut self.show_current_layout, ""));
 
-                ui.vertical_centered(|ui| {
-                    ui.heading("Router");
-                });
+                    Controls::section(ui, "Misc");
 
-                Controls::level_0_label(ui, "Costs");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("costs-grid").show(ui, |ui| {
-                        ui.label("Wire");
-                        ui.add(egui::DragValue::new(&mut self.wire_cost).clamp_range(0..=100));
-                        ui.end_row();
-
-                        ui.label("Strip");
-                        ui.add(egui::DragValue::new(&mut self.strip_cost).clamp_range(0..=100));
-                        ui.end_row();
-
-                        ui.label("Via");
-                        ui.add(egui::DragValue::new(&mut self.via_cost).clamp_range(0..=100));
-                        ui.end_row();
-
-                        ui.label("Cut");
-                        ui.add(egui::DragValue::new(&mut self.cut_cost).clamp_range(0..=100));
-                        ui.end_row();
-                    });
-                });
-
-                Controls::level_0_label(ui, "Display");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("display-grid").show(ui, |ui| {
-                        ui.label("Rat's Nest");
-                        ui.checkbox(&mut self.show_rats_nest, "");
-                        ui.end_row();
-
-                        ui.label("Only Failed");
-                        ui.checkbox(&mut self.show_only_failed, "");
-                        ui.end_row();
-
-                        ui.label("Current");
-                        ui.checkbox(&mut self.show_current_layout, "");
-                        ui.end_row();
-
-                        ui.label("Zoom");
-                        ui.add_sized(
-                            ui.available_size(),
-                            egui::Slider::new(&mut self.zoom, 1.0..=100.0).show_value(false),
-                        );
-                        ui.end_row();
-                    });
-                });
-
-                Controls::level_0_label(ui, "Misc");
-                ui.indent(1, |ui| {
-                    egui::Grid::new("misc-grid").show(ui, |ui| {
-                        ui.label("Pause");
-                        ui.checkbox(&mut self.pause_router, "");
-                        ui.end_row();
-                    });
+                    Controls::name_widget(ui, "Zoom", egui::DragValue::new(&mut self.zoom).clamp_range(1..=100));
+                    Controls::name_widget(ui, "Pause", egui::Checkbox::new(&mut self.pause_router, ""));
+                    Controls::name_float(ui, "ms/frame", self.ms_per_frame);
                 });
             });
         });
     }
 
-    fn level_0_label(ui: &mut Ui, s: &str) {
-        ui.add_space(6.0);
+    fn header(ui: &mut Ui, s: &str, first: bool) {
+        if !first {
+            ui.end_row();
+        }
+        ui.heading(s);
+        ui.add_space(ui.style().spacing.item_spacing.y / 2.0);
+        ui.end_row();
+    }
+
+    fn section(ui: &mut Ui, s: &str) {
         let text_color = ui.visuals().strong_text_color();
         let font_id = TextStyle::Button.resolve(ui.style());
         let galley = ui.fonts(|fonts| fonts.layout(s.to_string(), font_id, text_color, ui.available_width()));
         let (id, rect) = ui.allocate_space(galley.size());
-        ui.painter().galley(rect.min, galley);
-        // Right align
-        // let (response, painter) = ui.allocate_painter(ui.available_size(), egui::Sense::hover());
-        // let font = ui.fonts()[TextStyle::Body];
-        // let galley = ui.painter().layout_multiline(font, self.text.clone(), ui.available_width());
-        // let pos = response.rect.right_top() - vec2(galley.size().x, 0.0);
-        // painter.galley(pos, galley);
+        ui.painter().galley(rect.min, galley, Color32::BLACK);
+        ui.end_row();
+    }
+
+    fn name_int(ui: &mut Ui, name: &str, v: usize) {
+        ui.label(format!("    {}", name));
+        Controls::highlighted_label(ui, &format!("{}", v.to_formatted_string(&Locale::en)).to_owned());
+        ui.end_row();
+    }
+
+    fn name_float(ui: &mut Ui, name: &str, value: f32) {
+        ui.label(format!("    {}", name));
+        Controls::highlighted_label(ui, &format!("{:.2}", value));
+        ui.end_row();
+    }
+
+    fn name_widget(ui: &mut Ui, name: &str, widget: impl egui::Widget) {
+        ui.label(format!("    {}", name));
+        ui.add(widget);
+        ui.end_row();
     }
 
     fn highlighted_label(ui: &mut Ui, s: &str) {
-        let text_color = ui.visuals().strong_text_color();
-        let font_id = TextStyle::Button.resolve(ui.style());
-        let galley = ui.fonts(|fonts| fonts.layout(format!("  { }  ", s), font_id, text_color, ui.available_width()));
-        let (id, rect) = ui.allocate_space(galley.size());
-        // let color = Color32::from_white_alpha(128);
-        let color = ui.visuals().weak_text_color();
-        ui.painter().rect_filled(rect, 2.0, color);
-        ui.painter().galley(rect.min, galley);
+        ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
+            let bg_color = ui.visuals().weak_text_color();
+            ui.horizontal(|ui| {
+                // This adds space to the right, since we're in a right_to_left block.
+                ui.add_space(12.0);
+                let rich_text = egui::widgets::Label::new(
+                    egui::RichText::from(format!("  { }  ", s))
+                        .background_color(bg_color)
+                        .strong()
+                );
+                ui.add(rich_text);
+            });
+        });
     }
+
+    // Attempt at prettier version, but the rect size is incorrect on wider values.
+    // fn highlighted_label(ui: &mut Ui, s: &str) {
+    //     ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
+    //         let text_color = ui.visuals().strong_text_color();
+    //         let font_id = TextStyle::Button.resolve(ui.style());
+    //         let galley =
+    //             ui.fonts(|fonts| fonts.layout(format!("  { }  ", s), font_id, text_color, ui.available_width()));
+    //         let (id, rect) = ui.allocate_space(galley.size());
+    //         let color = ui.visuals().weak_text_color();
+    //         ui.painter().rect_filled(Rect::from_min_max(rect.left_top(), rect.right_bottom()), 2.0, color);
+    //         ui.painter().galley(rect.min, galley);
 }
